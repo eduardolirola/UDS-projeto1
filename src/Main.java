7236506bf233629c6cb7da3262c2bf9164ec50e2
= <=>
2018-06-09 13:48:15 -0300

    minha primeira aula

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+class Main {
+    public static void main(String[]args)
+
+    {
+        String nome="eduardo";
+        String sobrenome="Lirola";
+        Integer idade= 30;
+        Boolean sexoFeminino= false;
+        Double peso= 75.500;
+
+        System.out.println("Olá");
+        System.out.println("Meu nome é: "+ nome + " " + sobrenome);
+        System.out.println("Tenho: " + idade + " Anos Meu peso é: " + peso + "Kg");
+
+//        Verificação de idade
+
+        if (idade<=18) {
+            System.out.println("Novinho");
+        }
+
+        else if (idade>18 && idade<=30) {
+            System.out.println("Novão Ainda kk");
+        }
+
+        else if (idade>30) {
+            System.out.println("Tiozão");
+        }
+
+
+
+
+    }
+ }
+
---
+++ src/funcao.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class funcao {
+
+
+    public static String converteInteiroParaRomano (int numeroconvertido) {
+
+
+         switch(numeroconvertido){
+            case 1:
+                return  "I";
+            case 2:
+                return  "II";
+            case 3:
+                return  "III";
+            case 4:
+                return  "IV";
+            case 5:
+                return  "V";
+            case 6:
+                return  "VI";
+            case 7:
+                return  "VII";
+            case 8:
+                return  "VIII";
+            case 9:
+                return  "IX";
+            case 10:
+                return   "X";
+            default:
+                return  "Numeros corretos é de 1 a 10";
+        }
+}
+
+
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o numero:");
+        int retorno = scanner.nextInt();
+        System.out.println("O numero escolhido foi: " + retorno);
+        System.out.println(converteInteiroParaRomano(retorno));
+
+
+
+    }
+
+}
